0a1ceb58b87671001c1a4a9a6257786070cf8c7d
Asier <asier.lopezo@alumni.mondragon.edu>
2024-01-15 18:52:13 +0100

    init commit

---
+++ SampleCUnit_GitHub/src/source.c
@@ -0,0 +1,14 @@
+/*
+ * source.c
+ *
+ *  Created on: 29 nov. 2023
+ *      Author: asier
+ */
+
+#include "source.h"
+int add(int a, int b) {
+return a + b;
+}
+int subtract(int a, int b) {
+return a - b;
+}
